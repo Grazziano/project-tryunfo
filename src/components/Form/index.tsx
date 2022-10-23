109a78801345e9e3b94d82ab57e9c6d58879095f
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-10-23 17:44:25 -0300

    Creating Global style and form component

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './styled';
+
+function Form() {
+  return (
+    <Container>
+      <h1>FORM</h1>
+    </Container>
+  );
+}
+
+export default Form;
